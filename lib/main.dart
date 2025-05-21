c65c4397c2f530bbae129ac8eceda50741f94a5b
Ngoc Loc <hunga5160@gmail.com>
2025-05-21 21:01:03 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GitHub WebView',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const GitHubWebView(),
+    );
+  }
+}
+
+class GitHubWebView extends StatefulWidget {
+  const GitHubWebView({super.key});
+
+  @override
+  State<GitHubWebView> createState() => _GitHubWebViewState();
+}
+
+class _GitHubWebViewState extends State<GitHubWebView> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('http://192.168.1.16:3000'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(top: 20),
+        child: WebViewWidget(controller: controller),
+      ),
+    );
+  }
+}
